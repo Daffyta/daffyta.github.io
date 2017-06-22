6316c97cdf72b1e5d98549ba1485975ed7735d52
Daffyta <daffy.dine9@gmail.com>
2017-06-22 16:51:51 -0500

    Arreglando menú

--- js/main.js
+++ js/main.js
@@ -18,14 +18,15 @@ $(document).ready(function() {
   $('.collapsible').collapsible();
 });
 
-// Menu
-// 
-// $('.button-collapse').sideNav({
-//      menuWidth: 300, // Default is 300
-//      edge: 'right', // Choose the horizontal origin
-//      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-//      draggable: true, // Choose whether you can drag to open on touch screens,
-//      onOpen: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is opened
-//      onClose: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is closed
-//    }
-//  );
+window.onscroll = function (e) {
+    var habilidades = document.getElementById("habilidades");
+    var menuIcon = document.querySelector('.fa-2x');
+    var offset = habilidades.offsetTop;
+    var height = habilidades.clientHeight;
+    var scroll = window.scrollY;
+    if (offset <= scroll + 20 && scroll <= offset + height) {
+        menuIcon.style.color = '#406D8A';
+    } else {
+        menuIcon.style.color = '#f5f5f5';
+    }
+}
